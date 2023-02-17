c8f5d05d56b3f31dcc6d67a01c4bd63034e6a9a9
MdTehseenKhan <tehseenk435@gmail.com>
2023-02-17 16:26:42 +0500

    Celcius to fahrenheit

---
+++ 03-celcius_to_fahrenheit.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    float celcius;
+
+    printf("Enter temperature in celcius: ");
+    scanf("%f", &celcius);
+
+    float fahrenheit = (celcius * (9 / 5)) + 32;
+    printf("\nOn Fahrenheit Scale = %f F", fahrenheit);
+
+    return 0;
+}
